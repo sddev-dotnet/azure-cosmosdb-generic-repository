3653126ed30e07416290fefd80eb1e3174c4a634
= <=>
2023-04-16 10:31:59 -0700

    Add caching repository implementation

---
+++ SDDev.Net.GenericRepository.Tests/CachedRepositoryTests.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using SDDev.Net.GenericRepository.Caching;
+using SDDev.Net.GenericRepository.CosmosDB.Utilities;
+using SDDev.Net.GenericRepository.CosmosDB;
+using SDDev.Net.GenericRepository.Tests.TestModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+using static Azure.Core.HttpHeader;
+using Microsoft.Extensions.Caching.Memory;
+using FluentAssertions;
+
+namespace SDDev.Net.GenericRepository.Tests
+{
+    [TestClass]
+    public class CachedRepositoryTests
+    {
+        private static IConfiguration _config;
+        private static IOptions<CosmosDbConfiguration> _cosmos;
+        private static CosmosClient _client;
+        private static ILogger<GenericRepository<TestObject>> _logger;
+        private static ILoggerFactory _factory;
+        private CachedRepository<TestObject> _sut;
+        private IDistributedCache _cache;
+
+        [ClassInitialize]
+        public static async Task ClassInitialize(TestContext context)
+        {
+            var configBuilder = new ConfigurationBuilder();
+
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
+
+            _config = configBuilder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{environment}.json", true, true)
+                .AddEnvironmentVariables().Build();
+
+            var section = _config.GetSection("CosmosDb");
+            var cosmos = section.Get<CosmosDbConfiguration>();
+
+            _cosmos = new OptionsWrapper<CosmosDbConfiguration>(cosmos);
+
+            var serializer = new JsonSerializerSettings()
+            {
+                ContractResolver = new DefaultContractResolver(),
+                TypeNameHandling = TypeNameHandling.Objects,
+                Converters = new List<JsonConverter>() { new StringEnumConverter() }
+            };
+            //Serialize enums as string using default naming strategy (unchanged)
+            serializer.Converters.Add(new StringEnumConverter() { NamingStrategy = new DefaultNamingStrategy() });
+
+            _client = new CosmosClient(cosmos.ConnectionString, new CosmosClientOptions()
+            {
+                Serializer = new CosmosJsonDotNetSerializer(serializer)
+            });
+
+            _factory = new LoggerFactory();
+            _logger = _factory.CreateLogger<GenericRepository<TestObject>>();
+        }
+
+        [TestInitialize]
+        public async Task TestInit()
+        {
+            var opts = new MemoryDistributedCacheOptions();
+            var options = new OptionsWrapper<MemoryDistributedCacheOptions>(opts);
+            _cache = new MemoryDistributedCache(options);
+            _sut = new CachedRepository<TestObject>(_client, _logger, _cosmos, _cache, "TestContainer", "dhr");
+        }
+
+        [TestMethod]
+        public async Task WhenCreateItem_ThenCached()
+        {
+            // Arrange
+
+
+
+            // Act
+            var item = await _sut.Create(new TestObject() { Key = "test" });
+
+            // Assert
+            var cacheItem = await _cache.GetAsync(item.ToString());
+            cacheItem.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public async Task WhenGetItemFromCache_ThenItemReturnedFromCache()
+        {
+            // Arrange
+            var item = new TestObject() { Key = "test" };
+            await _sut.Create(item);
+
+            // Act
+            var result = await _sut.Get(item.Id.Value, item.PartitionKey);
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public async Task WhenItemDoesNotExistInCache_ThenRetrievedFromDB()
+        {
+            // Arrange
+            var item = new TestObject() { Key = "test" };
+            await _sut.Create(item);
+            await _cache.RemoveAsync(item.Id.ToString());
+
+            // Act
+            var result = await _sut.Get(item.Id.Value, item.PartitionKey);
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public async Task WhenItemUpdated_ThenCacheUpdated()
+        {
+            // Arrange
+            var item = new TestObject() { Key = "test", Prop1 = "Testing" };
+            await _sut.Create(item);
+            item.Prop1 = "Test Changed";
+
+            // Act
+            await _sut.Update(item);
+
+            // Assert
+            var result = await _cache.GetStringAsync(item.Id.ToString());
+            var testResult = JsonConvert.DeserializeObject<TestObject>(result);
+
+
+            testResult.Prop1.Should().Be("Test Changed");
+        }
+    }
+}
---
+++ SDDev.Net.GenericRepository/Caching/CachedRepository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using SDDev.Net.GenericRepository.Contracts.BaseEntity;
+using SDDev.Net.GenericRepository.Contracts.Repository;
+using SDDev.Net.GenericRepository.Contracts.Search;
+using SDDev.Net.GenericRepository.CosmosDB;
+using SDDev.Net.GenericRepository.CosmosDB.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDDev.Net.GenericRepository.Caching
+{
+    public class CachedRepository<T> : GenericRepository<T> where T : class, IStorableEntity
+    {
+        protected int CacheSeconds = 60;
+        protected bool refreshCache = true;
+        private IRepository<T> _repo;
+        private IDistributedCache _cache;
+
+        public CachedRepository(
+            CosmosClient client, 
+            ILogger<BaseRepository<T>> log, 
+            IOptions<CosmosDbConfiguration> config, 
+            IDistributedCache cache,
+            string collectionName = null, 
+            string databaseName = null, 
+            string partitionKey = null) : base(client, log, config, collectionName, databaseName, partitionKey)
+        {
+            _cache = cache;
+        }
+
+        public override async Task<Guid> Create(T model)
+        {
+            var item = await base.Create(model);
+
+            var options = new DistributedCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(CacheSeconds));
+            await _cache.SetAsync(model.Id.ToString(), Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(model)), options);
+
+            return item;
+        }
+
+        public override async Task Delete(Guid id, string partitionKey, bool force = false)
+        {
+            await _cache.RemoveAsync(id.ToString());
+            await base.Delete(id, partitionKey, force);
+        }
+
+        public override async Task<T> Get(Guid id, string partitionKey = null)
+        {
+            var item = await _cache.GetStringAsync(id.ToString());
+            if(item != null)
+            {
+                if (refreshCache)
+                {
+                    // reset the cache expiration because we retrieved the object
+                    _cache.Refresh(id.ToString());
+                }
+                var entity = JsonConvert.DeserializeObject<T>(item);
+                return entity;
+            }
+
+            return await base.Get(id, partitionKey);
+        }
+
+        public override async Task<Guid> Update(T model)
+        {
+            var options = new DistributedCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(CacheSeconds));
+            await _cache.SetStringAsync(model.Id.ToString(), JsonConvert.SerializeObject(model), options);
+
+            return await base.Update(model);
+        }
+    }
+}
