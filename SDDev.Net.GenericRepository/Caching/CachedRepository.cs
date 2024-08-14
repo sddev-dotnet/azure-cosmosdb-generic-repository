c20b7722ae868fd1221d88af0ac09dfac16db069
Dwayne Hunter <dhunter@kinectify.com>
2024-08-14 16:23:51 -0400

    revert block-scoped namespace because of terrible github diff

--- SDDev.Net.GenericRepository/Caching/CachedRepository.cs
+++ SDDev.Net.GenericRepository/Caching/CachedRepository.cs
@@ -12,162 +12,163 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SDDev.Net.GenericRepository.Caching;
-
-public class CachedRepository<T> : ICachedRepository<T> where T : class, IStorableEntity
+namespace SDDev.Net.GenericRepository.Caching
 {
-    protected int cacheSeconds = 60;
-    protected bool refreshCache = true;
-    private IRepository<T> _repo;
-    private IDistributedCache _cache;
-
-    public CachedRepository(
-        ILogger<BaseRepository<T>> log,
-        IOptions<CosmosDbConfiguration> config,
-        IRepository<T> repository,
-        IDistributedCache cache,
-        int cacheSeconds = 60,
-        bool refreshCache = true
-        )
+    public class CachedRepository<T> : ICachedRepository<T> where T : class, IStorableEntity
     {
-        _cache = cache;
-        this.cacheSeconds = cacheSeconds;
-        this.refreshCache = refreshCache;
-        _repo = repository;
-    }
+        protected int cacheSeconds = 60;
+        protected bool refreshCache = true;
+        private IRepository<T> _repo;
+        private IDistributedCache _cache;
 
-    public async Task<Guid> Create(T model)
-    {
-        var item = await _repo.Create(model);
-
-        var options = GetCacheEntryOptions();
-        await _cache.SetAsync(model.Id.ToString(), Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(model)), options);
-
-        return item;
-    }
-
-    public async Task Delete(Guid id, string partitionKey, bool force = false)
-    {
-        await _cache.RemoveAsync(id.ToString());
-        await _repo.Delete(id, partitionKey, force);
-    }
-
-    public Task Delete(T model, bool force = false)
-    {
-        return Delete(model.Id.Value, model.PartitionKey, force);
-    }
-
-    public async Task<T> Get(Guid id, string partitionKey = null)
-    {
-        var item = await _cache.GetStringAsync(id.ToString());
-        if (item != null)
+        public CachedRepository(
+            ILogger<BaseRepository<T>> log,
+            IOptions<CosmosDbConfiguration> config,
+            IRepository<T> repository,
+            IDistributedCache cache,
+            int cacheSeconds = 60,
+            bool refreshCache = true
+            )
         {
-            var entity = JsonConvert.DeserializeObject<T>(item);
-            return entity;
+            _cache = cache;
+            this.cacheSeconds = cacheSeconds;
+            this.refreshCache = refreshCache;
+            _repo = repository;
         }
 
-        var result = await _repo.Get(id, partitionKey);
+        public async Task<Guid> Create(T model)
+        {
+            var item = await _repo.Create(model);
 
-        if (result != null)
+            var options = GetCacheEntryOptions();
+            await _cache.SetAsync(model.Id.ToString(), Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(model)), options);
+
+            return item;
+        }
+
+        public async Task Delete(Guid id, string partitionKey, bool force = false)
+        {
+            await _cache.RemoveAsync(id.ToString());
+            await _repo.Delete(id, partitionKey, force);
+        }
+
+        public Task Delete(T model, bool force = false)
+        {
+            return Delete(model.Id.Value, model.PartitionKey, force);
+        }
+
+        public async Task<T> Get(Guid id, string partitionKey = null)
+        {
+            var item = await _cache.GetStringAsync(id.ToString());
+            if (item != null)
+            {
+                var entity = JsonConvert.DeserializeObject<T>(item);
+                return entity;
+            }
+
+            var result = await _repo.Get(id, partitionKey);
+
+            if (result != null)
+            {
+                var options = GetCacheEntryOptions();
+                await _cache.SetAsync(result.Id.ToString(), Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(result)), options);
+            }
+
+            return result;
+        }
+
+        public Task<ISearchResult<T>> Get(Expression<Func<T, bool>> predicate, ISearchModel model)
+        {
+            return _repo.Get(predicate, model);
+        }
+
+        public Task<ISearchResult<T>> Get(string query, ISearchModel model)
+        {
+            return _repo.Get(query, model);
+        }
+
+        public Task<ISearchResult<T>> GetAll(Expression<Func<T, bool>> predicate, ISearchModel model)
+        {
+            return _repo.GetAll(predicate, model);
+        }
+
+        public async Task<Guid> Update(T model)
         {
             var options = GetCacheEntryOptions();
-            await _cache.SetAsync(result.Id.ToString(), Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(result)), options);
+            await _cache.SetStringAsync(model.Id.ToString(), JsonConvert.SerializeObject(model), options);
+
+            return await _repo.Update(model);
         }
 
-        return result;
-    }
-
-    public Task<ISearchResult<T>> Get(Expression<Func<T, bool>> predicate, ISearchModel model)
-    {
-        return _repo.Get(predicate, model);
-    }
-
-    public Task<ISearchResult<T>> Get(string query, ISearchModel model)
-    {
-        return _repo.Get(query, model);
-    }
-
-    public Task<ISearchResult<T>> GetAll(Expression<Func<T, bool>> predicate, ISearchModel model)
-    {
-        return _repo.GetAll(predicate, model);
-    }
-
-    public async Task<Guid> Update(T model)
-    {
-        var options = GetCacheEntryOptions();
-        await _cache.SetStringAsync(model.Id.ToString(), JsonConvert.SerializeObject(model), options);
-
-        return await _repo.Update(model);
-    }
-
-    public async Task<Guid> Upsert(T model)
-    {
-        var options = GetCacheEntryOptions();
-        await _cache.SetStringAsync(model.Id.ToString(), JsonConvert.SerializeObject(model), options);
-        return await _repo.Upsert(model);
-    }
-
-    public async Task<T> FindOne(Expression<Func<T, bool>> predicate, string partitionKey = null, bool singleResult = false)
-    {
-
-        var result = await _repo.FindOne(predicate, partitionKey, singleResult).ConfigureAwait(false);
-
-
-        if (result != null)
+        public async Task<Guid> Upsert(T model)
         {
             var options = GetCacheEntryOptions();
-            await _cache.SetAsync(result.Id.ToString(), Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(result)), options);
+            await _cache.SetStringAsync(model.Id.ToString(), JsonConvert.SerializeObject(model), options);
+            return await _repo.Upsert(model);
         }
 
-
-        return result;
-    }
-
-    public async Task Evict(string key)
-    {
-        await _cache.RemoveAsync(key);
-    }
-
-    public Task Cache<T1>(T1 entity, string key)
-    {
-        return _cache.SetAsync(
-            key,
-            Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(entity)),
-            GetCacheEntryOptions()
-        );
-    }
-
-    public async Task<Model> Retrieve<Model>(string id)
-    {
-        var item = await _cache.GetStringAsync(id);
-        if (item != null)
+        public async Task<T> FindOne(Expression<Func<T, bool>> predicate, string partitionKey = null, bool singleResult = false)
         {
-            Model entity = JsonConvert.DeserializeObject<Model>(item);
-            return entity;
+
+            var result = await _repo.FindOne(predicate, partitionKey, singleResult).ConfigureAwait(false);
+
+
+            if (result != null)
+            {
+                var options = GetCacheEntryOptions();
+                await _cache.SetAsync(result.Id.ToString(), Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(result)), options);
+            }
+
+
+            return result;
         }
 
-        return default;
-    }
-
-    public Task<dynamic> Retrieve(string key)
-    {
-        return Retrieve<dynamic>(key);
-    }
-
-    public Task<int> Count(Expression<Func<T, bool>> predicate, string partitionKey = null)
-    {
-        return _repo.Count(predicate, partitionKey);
-    }
-
-    private DistributedCacheEntryOptions GetCacheEntryOptions()
-    {
-        var options = new DistributedCacheEntryOptions();
-
-        if (refreshCache)
+        public async Task Evict(string key)
         {
-            options.SetSlidingExpiration(TimeSpan.FromSeconds(cacheSeconds));
+            await _cache.RemoveAsync(key);
         }
 
-        return options;
+        public Task Cache<T1>(T1 entity, string key)
+        {
+            return _cache.SetAsync(
+                key,
+                Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(entity)),
+                GetCacheEntryOptions()
+            );
+        }
+
+        public async Task<Model> Retrieve<Model>(string id)
+        {
+            var item = await _cache.GetStringAsync(id);
+            if (item != null)
+            {
+                Model entity = JsonConvert.DeserializeObject<Model>(item);
+                return entity;
+            }
+
+            return default;
+        }
+
+        public Task<dynamic> Retrieve(string key)
+        {
+            return Retrieve<dynamic>(key);
+        }
+
+        public Task<int> Count(Expression<Func<T, bool>> predicate, string partitionKey = null)
+        {
+            return _repo.Count(predicate, partitionKey);
+        }
+
+        private DistributedCacheEntryOptions GetCacheEntryOptions()
+        {
+            var options = new DistributedCacheEntryOptions();
+
+            if (refreshCache)
+            {
+                options.SetSlidingExpiration(TimeSpan.FromSeconds(cacheSeconds));
+            }
+
+            return options;
+        }
     }
 }
